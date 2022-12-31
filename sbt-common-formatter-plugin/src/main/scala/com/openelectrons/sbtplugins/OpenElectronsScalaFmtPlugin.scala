c8fc2c0badc6e08fd30d6f8fcff14390c9485284
joesan <codeintheopen@gmail.com>
2022-12-31 08:07:12 +0100

    'added changes'

--- sbt-common-formatter-plugin/src/main/scala/com/openelectrons/sbtplugins/OpenElectronsScalaFmtPlugin.scala
+++ sbt-common-formatter-plugin/src/main/scala/com/openelectrons/sbtplugins/OpenElectronsScalaFmtPlugin.scala
@@ -1,27 +1,59 @@
 package com.openelectrons.sbtplugins
 
 import sbt._
-import Keys._
 
 object OpenElectronsScalaFmtPlugin extends AutoPlugin {
+
+  val commonScalaFormatConfig: String =
+    """
+      |version = 3.6.1
+      |runner.dialect = scala213source3
+      |
+      |project.git = true
+      |preset = default
+      |
+      |align.preset = none
+      |align.tokens = [
+      |  {code = "<-"},
+      |]
+      |
+      |docstrings.style = keep
+      |maxColumn = 120
+      |
+      |rewrite.rules = [
+      |  SortImports,
+      |  AvoidInfix,
+      |]
+      |
+      |spaces.inImportCurlyBraces = true
+      |includeNoParensInSelectChains = false
+      |trailingCommas = preserve
+      |
+      |continuationIndent {
+      |  callSite = 2
+      |  defnSite = 2
+      |  extendSite = 2
+      |}
+      |
+      |optIn {
+      |  forceBlankLineBeforeDocstring = false
+      |}
+      |
+      |newlines {
+      |  source = keep
+      |  afterCurlyLambdaParams = preserve
+      |  beforeCurlyLambdaParams = multilineWithCaseOnly
+      |  topLevelBodyIfMinStatements = []
+      |}
+      |""".stripMargin
+
   override def trigger = allRequirements
   override def requires = plugins.JvmPlugin
   override def buildSettings: Seq[Def.Setting[_]] = {
     SettingKey[Unit]("scalafmtGenerateConfig") :=
       IO.write(
         // writes to file once when build is loaded
-        file(".scalafmt-common.conf"),
-        ("version = 3.6.1\n" +
-          "runner.dialect = scala213source3\n\n" +
-          "project.git = true\n" +
-          "preset = default\n\n" +
-          "align.preset = none\n" +
-          "align.tokens = [\n" +
-          "  {code = \"<-\"},\n" +
-          "]\n\n" +
-          "docstrings.style = keep\n" +
-          "maxColumn = 120\n\n"
-          ).stripMargin.getBytes("UTF-8")
+        file(".scalafmt-common.conf"), commonScalaFormatConfig.stripMargin.getBytes("UTF-8")
       )
   }
 }
